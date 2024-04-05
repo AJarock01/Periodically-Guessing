56b56ce1283442f7760acd9b4c31e6dc22f121c7
AJarock01 <146214568+AJarock01@users.noreply.github.com>
2024-04-05 12:17:37 -0600

    Add files via upload

---
+++ Periodically Geussing/script.js
@@ -0,0 +1,192 @@
+// Event Listeners
+document.getElementById("Randomize").addEventListener("click", randomElement);
+
+let elementSignEl = document.getElementById("elementSign");
+
+function randomElement() {
+  let rand = Math.floor(Math.random() * 118 + 1);
+
+  if (rand === 1) {
+    elementSignEl.innerHTML = "H";
+  } else if (rand === 2) {
+    elementSignEl.innerHTML = "Li";
+  } else if (rand === 3) {
+    elementSignEl.innerHTML = "Be";
+  } else if (rand === 4) {
+    elementSignEl.innerHTML = "Na";
+  } else if (rand === 5) {
+    elementSignEl.innerHTML = "Mg";
+  } else if (rand === 6) {
+    elementSignEl.innerHTML = "K";
+  } else if (rand === 7) {
+    elementSignEl.innerHTML = "Ca";
+  } else if (rand === 8) {
+    elementSignEl.innerHTML = "Rb";
+  } else if (rand === 9) {
+    elementSignEl.innerHTML = "Sr";
+  } else if (rand === 10) {
+    elementSignEl.innerHTML = "Cs";
+  } else if (rand === 11) {
+    elementSignEl.innerHTML = "Ba";
+  } else if (rand === 12) {
+    elementSignEl.innerHTML = "Fr";
+  } else if (rand === 13) {
+    elementSignEl.innerHTML = "Ra";
+  } else if (rand === 14) {
+    elementSignEl.innerHTML = "Sc";
+  } else if (rand === 15) {
+    elementSignEl.innerHTML = "Y";
+  } else if (rand === 16) {
+    elementSignEl.innerHTML = "Lu";
+  } else if (rand === 17) {
+    elementSignEl.innerHTML = "Lr";
+  } else if (rand === 18) {
+    elementSignEl.innerHTML = "Ti";
+  } else if (rand === 19) {
+    elementSignEl.innerHTML = "Zr";
+  } else if (rand === 20) {
+    elementSignEl.innerHTML = "Hf";
+  } else if (rand === 21) {
+    elementSignEl.innerHTML = "Rf";
+  } else if (rand === 22) {
+    elementSignEl.innerHTML = "V";
+  } else if (rand === 23) {
+    elementSignEl.innerHTML = "Nb";
+  } else if (rand === 24) {
+    elementSignEl.innerHTML = "Ta";
+  } else if (rand === 25) {
+    elementSignEl.innerHTML = "Db";
+  } else if (rand === 26) {
+    elementSignEl.innerHTML = "Cr";
+  } else if (rand === 27) {
+    elementSignEl.innerHTML = "Mo";
+  } else if (rand === 28) {
+    elementSignEl.innerHTML = "W";
+  } else if (rand === 29) {
+    elementSignEl.innerHTML = "Sg";
+  } else if (rand === 30) {
+    elementSignEl.innerHTML = "Mn";
+  } else if (rand === 31) {
+    elementSignEl.innerHTML = "Tc";
+  } else if (rand === 32) {
+    elementSignEl.innerHTML = "Re";
+  } else if (rand === 33) {
+    elementSignEl.innerHTML = "Bh";
+  } else if (rand === 34) {
+    elementSignEl.innerHTML = "Fe";
+  } else if (rand === 35) {
+    elementSignEl.innerHTML = "Ru";
+  } else if (rand === 36) {
+    elementSignEl.innerHTML = "Os";
+  } else if (rand === 37) {
+    elementSignEl.innerHTML = "Hs";
+  } else if (rand === 38) {
+    elementSignEl.innerHTML = "Co";
+  } else if (rand === 39) {
+    elementSignEl.innerHTML = "Rh";
+  } else if (rand === 40) {
+    elementSignEl.innerHTML = "Ir";
+  } else if (rand === 41) {
+    elementSignEl.innerHTML = "Mt";
+  } else if (rand === 42) {
+    elementSignEl.innerHTML = "Ni";
+  } else if (rand === 43) {
+    elementSignEl.innerHTML = "Pd";
+  } else if (rand === 44) {
+    elementSignEl.innerHTML = "Pt";
+  } else if (rand === 45) {
+    elementSignEl.innerHTML = "Ds";
+  } else if (rand === 46) {
+    elementSignEl.innerHTML = "Cu";
+  } else if (rand === 47) {
+    elementSignEl.innerHTML = "Ag";
+  } else if (rand === 48) {
+    elementSignEl.innerHTML = "Au";
+  } else if (rand === 49) {
+    elementSignEl.innerHTML = "Rg";
+  } else if (rand === 50) {
+    elementSignEl.innerHTML = "Zn";
+  } else if (rand === 51) {
+    elementSignEl.innerHTML = "Cd";
+  } else if (rand === 52) {
+    elementSignEl.innerHTML = "Hg";
+  } else if (rand === 53) {
+    elementSignEl.innerHTML = "Cn";
+  } else if (rand === 54) {
+    elementSignEl.innerHTML = "B";
+  } else if (rand === 55) {
+    elementSignEl.innerHTML = "C";
+  } else if (rand === 56) {
+    elementSignEl.innerHTML = "N";
+  } else if (rand === 57) {
+    elementSignEl.innerHTML = "O";
+  } else if (rand === 58) {
+    elementSignEl.innerHTML = "F";
+  } else if (rand === 59) {
+    elementSignEl.innerHTML = "Ne";
+  } else if (rand === 60) {
+    elementSignEl.innerHTML = "He";
+  } else if (rand === 61) {
+    elementSignEl.innerHTML = "Ai";
+  } else if (rand === 62) {
+    elementSignEl.innerHTML = "Si";
+  } else if (rand === 63) {
+    elementSignEl.innerHTML = "P";
+  } else if (rand === 64) {
+    elementSignEl.innerHTML = "S";
+  } else if (rand === 65) {
+    elementSignEl.innerHTML = "Ci";
+  } else if (rand === 66) {
+    elementSignEl.innerHTML = "Ar";
+  } else if (rand === 67) {
+    elementSignEl.innerHTML = "Ga";
+  } else if (rand === 68) {
+    elementSignEl.innerHTML = "Ge";
+  } else if (rand === 69) {
+    elementSignEl.innerHTML = "As";
+  } else if (rand === 70) {
+    elementSignEl.innerHTML = "Se";
+  } else if (rand === 71) {
+    elementSignEl.innerHTML = "Br";
+  } else if (rand === 72) {
+    elementSignEl.innerHTML = "Kr";
+  } else if (rand === 73) {
+    elementSignEl.innerHTML = "In";
+  } else if (rand === 74) {
+    elementSignEl.innerHTML = "Sn";
+  } else if (rand === 75) {
+    elementSignEl.innerHTML = "Sb";
+  } else if (rand === 76) {
+    elementSignEl.innerHTML = "Te";
+  } else if (rand === 77) {
+    elementSignEl.innerHTML = "I";
+  } else if (rand === 78) {
+    elementSignEl.innerHTML = "Xe";
+  } else if (rand === 79) {
+    elementSignEl.innerHTML = "Ti";
+  } else if (rand === 80) {
+    elementSignEl.innerHTML = "Pb";
+  } else if (rand === 81) {
+    elementSignEl.innerHTML = "Bi";
+  } else if (rand === 82) {
+    elementSignEl.innerHTML = "Po";
+  } else if (rand === 83) {
+    elementSignEl.innerHTML = "At";
+  } else if (rand === 84) {
+    elementSignEl.innerHTML = "Rn";
+  } else if (rand === 85) {
+    elementSignEl.innerHTML = "Nh";
+  } else if (rand === 86) {
+    elementSignEl.innerHTML = "Fi";
+  } else if (rand === 87) {
+    elementSignEl.innerHTML = "Mc";
+  } else if (rand === 88) {
+    elementSignEl.innerHTML = "Lv";
+  } else if (rand === 89) {
+    elementSignEl.innerHTML = "Ts";
+  } else if (rand === 90) {
+    elementSignEl.innerHTML = "Og";
+  } else if (rand === 91) {
+    elementSignEl.innerHTML = "La";
+  }
+}
